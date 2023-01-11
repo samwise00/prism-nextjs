1bf448acda1f7ecd0b4d6eaeb680ecb4f64c8628
samwise00 <sam.sandoval.00@gmail.com>
2023-01-11 01:02:37 -0500

    test

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,34 +6,7 @@ import styles from "../styles";
 import { navVariants } from "../utils/motion";
 import Link from "next/link";
 
-import { resolve } from "url";
-
-import React, { useMemo } from "react";
-
 import { ConnectButton } from "web3uikit";
-
-const BaseLink = ({ href, as, ...rest }) => {
-  const newAs = useMemo(() => {
-    let baseURI_as = as || href;
-
-    // make absolute url relative
-    // when displayed in url bar
-    if (baseURI_as.startsWith("/")) {
-      //  for static html compilation
-      baseURI_as = "." + href;
-      // <IPFSLink href="/about"> => <a class="jsx-2055897931" href="./about">About</a>
-
-      // on the client
-      if (typeof document !== "undefined") {
-        baseURI_as = resolve(document.baseURI, baseURI_as);
-        // => <a href="https://gateway.ipfs.io/ipfs/Qm<hash>/about">About</a>
-      }
-    }
-    return baseURI_as;
-  }, [as, href]);
-
-  return <Link {...rest} href={href} as={newAs} />;
-};
 
 const Navbar = () => (
   <div className="bg-black">
@@ -47,20 +20,20 @@ const Navbar = () => (
         className={`${styles.innerWidth} mx-auto flex justify-between md:gap-16 gap-4`}
         id="navbar"
       >
-        <BaseLink href="./">
+        <Link href="/">
           <h2 className="font-geo font-extrabold py-2 md:text-[30px] text-[24px] leading-[30.24px] text-white cursor-pointer">
             Prism<span className="text-pink-500">.</span>
           </h2>
-        </BaseLink>
+        </Link>
         <div className="text-white flex flex-row">
           <div className="flex md:flex-row gap-2">
             <div className="text-white flex flex-row md:gap-6 gap-2 mt-4">
-              <BaseLink legacyBehavior href="./trade">
+              <Link legacyBehavior href="/trade" replace>
                 <h3 className="text-xs cursor-pointer">Trade</h3>
-              </BaseLink>
-              <BaseLink legacyBehavior href="./stake">
+              </Link>
+              <Link legacyBehavior href="/stake" replace>
                 <h3 className="text-xs cursor-pointer">Stake</h3>
-              </BaseLink>
+              </Link>
             </div>
           </div>
           <div className="flex justify-start items-center mx-auto mb-4">
